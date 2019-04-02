cead57956dd086a2fc3b06af6b38785e839a33b8
Marce Arratia <marratiaf@hotmail.com>
2019-04-02 00:09:01 -0300

    agregando boton zona segura

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-window.onload("myalert()");
+console.log("Hola");
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
-function myalert(){
-    alert('funciona?')
-}
+
+
+document.getElementById("zoneButton").addEventListener("click",function(){
+
+    document.getElementById("buttonZoneuno").innerHTML=` <img src="img/Group 4.png"></img>`
+});
 
 /**
  * Moves the map to display over Berlin
  *
- * @param  {H.Map} map      A HERE Map instance within the application
+ *   A HERE Map instance within the application
 
 function moveMapToBerlin(map){
     map.setCenter({lat:52.5159, lng:13.3777});
@@ -24,5 +30,4 @@ function moveMapToRecoleta(map){
   
   }
 */
-
 
